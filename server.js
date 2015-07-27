35e26c00d480099ee2e2713c5b298bd2316d08f6
Rafael Zaccanini <rafael.zaccanini@gmail.com>
2015-07-27 16:53:52 -0300

    test

--- server.js
+++ server.js
@@ -4,19 +4,26 @@ var io = require('socket.io')(http); //, {'transports': ['websocket', 'xhr-polli
 var cpu = require('./cpu.js');
 var interval = 1000;
 var port = process.env.PORT || 80;
+var connectCounter = 0;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
  
 setInterval(function(){
-	
-	//io.emit('test', io.sockets.manager.server.connections);
-	
 	 cpu.getPercentageUsage(interval, function(percentage){
 	 	io.emit('cpu_usage', percentage);
 	 });
 }, interval);
+ 
+io.on('connect', function() { 
+	connectCounter++;
+	
+	io.emit('test', connectCounter);
+	
+	if(connectCounter == 7)
+	 	io.disconnect();
+});
  
 http.listen(port, function(){
   console.log('Listening');
