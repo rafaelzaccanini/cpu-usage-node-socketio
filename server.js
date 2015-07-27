0d0712f00b394480521891f99b9d50559a098095
Rafael Zaccanini <rafael.zaccanini@gmail.com>
2015-07-27 16:43:53 -0300

    test

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var app = require('express')();
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
+// app.use(function(req, res, next) {
+//         res.header("Access-Control-Allow-Origin", "*");
+//         res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//         res.header("Access-Control-Allow-Headers", "Content-Type");
+//         res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+//         next();
+//     });
 	
 var http = require('http').Server(app);
 var io = require('socket.io')(http); //, {'transports': ['websocket', 'xhr-polling']});
