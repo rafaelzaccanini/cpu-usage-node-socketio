9cb2b32301461cb52333f87a1214772226da29a9
Rafael Zaccanini <rafael.zaccanini@gmail.com>
2015-07-27 15:31:41 -0300

    setting port and io to azure deploy

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//var cpu = require('./cpu.js');
+var cpu = require('./cpu.js');
 var interval = 1000;
 var port = process.env.PORT || 80;
 
-// io.configure(function () {
-//    io.set('transports', ['websocket','xhr-polling'])
-// });
- 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
  
 setInterval(function(){
 	io.emit('cpu_usage', '500');
-	// cpu.getPercentageUsage(interval, function(percentage){
-	// 	io.emit('cpu_usage', percentage);
-	// });
+	 cpu.getPercentageUsage(interval, function(percentage){
+	 	io.emit('cpu_usage', percentage);
+	 });
 }, interval);
  
 http.listen(port, function(){
-  console.log('Listening on *:3500');
+  console.log('Listening');
 });
-
-// http.createServer(function(req, res) {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.sendFile(__dirname + '/index.html');
-// }).listen(port);
